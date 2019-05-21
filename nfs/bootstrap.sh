57701abecdfc5665cbe275035a18cc76a56d6b4b
li <lizhigang858@126.com>
2019-05-21 18:27:41 +0800

    remove guest

--- nfs/bootstrap.sh
+++ nfs/bootstrap.sh
@@ -23,6 +23,9 @@ echo "root" | passwd --stdin root >/dev/null 2>&1
 echo "[TASK 5] Install nfs-utils"
 echo yum install -y -q nfs-utils
 
+
+
+
 #Config /etc/exports
 echo "[TASK 5] /etc/exports可以参考鸟哥"
 cat >> /etc/exports<<EOF
@@ -33,6 +36,9 @@ cat >> /etc/exports<<EOF
 #我要讓 *.example.com 網域的主機，登入我的 NFS 主機時，可以存取 /home/linux ，但是他們存資料的時候，我希望他們的 UID 與 GID 都變成 45 這個身份的使用者，假設我 NFS 伺服器上的 UID 45 與 GID 45 的用戶/群組名稱為 nfsanon
 /home/linux   *.example.com(rw,all_squash,anonuid=45,anongid=45)
 EOF
+
+
+
 
 # Start rpcbind and nfs
 echo "[TASK 5] 启动rpcbind和nfs"
